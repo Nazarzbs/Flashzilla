4e05cbe3a0e9eaab78981c5aa25656dfb7402dd3
Nazar <famina1998@gmail.com>
2024-09-01 21:40:24 +0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Nazar on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
