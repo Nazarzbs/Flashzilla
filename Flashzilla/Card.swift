32a39163d0217861ef2d28e1474f665b23ee75f4
Nazar <famina1998@gmail.com>
2024-09-08 21:08:04 +0300

    App without challenges

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Nazar on 2/9/24.
+//
+
+import Foundation
+
+struct Card: Codable {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
+
+
